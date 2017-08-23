94bb1353c6b5db8f1c442c88a613bfed702d5a4a
neo.abi1000 <neo.abi1000@gmail.com>
2017-08-24 04:56:49 +0530

    style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,26 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>This is the about page</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React, which is to check out the weather of different cities.
+      </p>
+      <p>
+        I am using the the openweathermap api for getting the exact weather data available for different cities.
+        So all my weather data are authentic. Check out my app on heroku web server site:
+        <a href="http://dry-tundra-33736.herokuapp.com/#/?_k=7h1clu">My Weather App</a>
+      </p>
+      <p>
+        Here are the some the tools which I have used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - A JavaScript framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org/">Open Weather map</a> - used for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
